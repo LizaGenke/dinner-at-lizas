b7f751bcaf921a499b4bca88c0fc2931549da9c2
Berwout de Vries Robles <bdvr@live.nl>
2019-10-13 21:30:46 +0200

    Amazing code of Berwout reads meals from list now

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ class Ingredient:
     def describe(self):
         return self.name
 
-
 class Meal:
     def __init__(self, name, contents):
         self.name = name
@@ -18,7 +17,6 @@ class Meal:
             description += ingredient_quantity[0].describe() + '\n'
         return description
 
-
 class Quantity:
     def __init__(self, amount, metric=None):
         self.amount = amount
@@ -30,11 +28,29 @@ class Quantity:
             description += ' ' + self.metric
         return description
 
-egg = Ingredient('egg')
-bellpepper = Ingredient('bellpepper')
+meals_list = []
+current_meal_name = None
 
-q_eggs = Quantity(2, 'gr')
-q_bellpepper = Quantity(1)
+f_meals = open('meals.txt', 'r')
+for line in f_meals:
+    if ':' in line:
+        if current_meal_name != None:
+            meals_list.append(Meal(current_meal_name, current_meal_contents))
+            current_meal_name = None
+            current_meal_contents = []
 
-wonderful_Berwout_breakfast = Meal('wonderful Berwout breakfast',[(egg,q_eggs),(bellpepper,q_bellpepper)])
-print(wonderful_Berwout_breakfast.describe())
+        current_meal_name = line.replace(':','').strip()
+        current_meal_contents = []
+
+    if '|' in line:
+        ingredient_quantity_list = line.split('|')
+        current_metric = None
+        if (len(ingredient_quantity_list)>2):
+            current_metric=ingredient_quantity_list[2].strip()
+
+        current_meal_contents.append((Ingredient(ingredient_quantity_list[0].strip()), Quantity(ingredient_quantity_list[1].strip() ,current_metric)))
+
+meals_list.append(Meal(current_meal_name, current_meal_contents))
+
+for meal in meals_list:
+    print(meal.describe())
